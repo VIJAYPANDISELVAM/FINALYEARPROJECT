a23c842776a988610105b6e5cfe3ce8c4d5c636d
VIJAYPANDISELVAM <vijaypandiselvam01@gmail.com>
2026-02-10 15:27:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // CRONOS - Enhanced UI Compatible Script
 // Production Configuration - Vercel + Render
 
-let lastAnalysisResult = 1;
+let lastAnalysisResult = null;
 let lastReportId = null;
 let currentMode = null;
 let analysisInProgress = false;
